5658c31bae53ca7622fcb001c96bd4e1865b6826
Zehua-Lin <lzh.developer@gmail.com>
2017-04-06 00:33:15 +1000

    Revert "Merge pull request #3 from rmit-Errol/Redesign-Menu-Scene"
    This reverts commit e85a5cae5e06248e843c6f8ff1a58c2321915a63, reversing
    changes made to 3e76c435f32c5aaf0df2f98ee634f2926c35853b.

--- Chirality/Assets/Script/MenuManager.cs
+++ Chirality/Assets/Script/MenuManager.cs
@@ -13,16 +13,16 @@ public class MenuManager : MonoBehaviour {
 	[SerializeField] Toggle backgroundMusicToggle;
 	[SerializeField] Toggle soundEffectToggle;
 	[SerializeField] Toggle leftHandModeToggle;
+	[SerializeField] Button levelTwoButton;
 	[SerializeField] GameObject levelTwoPanel;
 	[SerializeField] Animator scrollAnimation;
-	[SerializeField] Animator levelTwoSubMenu;
-	[SerializeField] Animator levelFourSubMenu;
 
 	private GameObject backgroundMusicObject = null;	// this is the actual audio object in game
 	private GameObject soundEffectObject = null;
 
 	void Start() {
 		settingPanel.SetActive(false);
+		hideLevelTwoPanel();
 		
 		loadUserSetting();
 			
@@ -58,27 +58,13 @@ public class MenuManager : MonoBehaviour {
 	}
 
 	public void displayLevelTwoPanel() {
-		// if(!levelFourSubMenu.GetBool("isHidden")) {
-		// 	hideLevelFourPanel();
-		// }
-		hideLevelFourPanel();
-		levelTwoSubMenu.SetBool("isHidden",false);
+		levelTwoButton.gameObject.SetActive(false);
+		levelTwoPanel.SetActive(true);
 	}
 
 	public void hideLevelTwoPanel() {
-		levelTwoSubMenu.SetBool("isHidden",true);
-	}
-
-	public void displayLevelFourPanel() {
-		// if(!levelTwoSubMenu.GetBool("isHidden")) {
-		// 	hideLevelTwoPanel();
-		// }
-		hideLevelTwoPanel();
-		levelFourSubMenu.SetBool("isHidden",false);
-	}
-
-	public void hideLevelFourPanel() {
-		levelFourSubMenu.SetBool("isHidden",true);
+		levelTwoButton.gameObject.SetActive(true);
+		levelTwoPanel.SetActive(false);
 	}
 
 	public void stopScrolling() {
