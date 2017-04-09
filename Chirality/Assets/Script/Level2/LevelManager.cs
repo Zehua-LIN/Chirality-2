46645446b7483efc71d0c3f48b39ae4692f8ecea
amt1986 <amt1986@gmail.com>
2017-04-09 23:37:08 +1000

    Level 2
    Added Level 1 Prototype

---
+++ Chirality/Assets/Script/Level2/LevelManager.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+
+public class LevelManager : MonoBehaviour {
+
+
+	[SerializeField]
+	private int _state;
+
+
+	public Sprite[] tileFace;
+	public Sprite[] tileName;
+	public Sprite tileBack;
+	public GameObject[] tiles;
+	public Text matchText;
+
+	private bool _init = false;
+	private int _matches = 27;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!_init)
+			initializeCards ();
+		if (Input.GetMouseButtonUp (0))
+			checkCards ();
+	}
+
+
+	void initializeCards() {
+
+
+		var rnd = new System.Random();
+		var randomNumbers = Enumerable.Range(0,54).OrderBy(x => rnd.Next()).Take(54).ToList();
+
+		//Debug.Log(randomNumbers.Count);
+		for ( int i = 0; i<randomNumbers.Count; i++) {
+			Debug.Log(randomNumbers[i]);
+
+		}
+
+			
+		for (int id = 0; id < 12; id++) {
+			tiles [randomNumbers[id]].GetComponent<Tile> ().cardValue = id; 
+		}
+		for (int id = 12; id < 24; id++) {
+			tiles [randomNumbers[id]].GetComponent<Tile> ().cardValue = id-12; 
+			tiles [randomNumbers[id]].GetComponent<Tile> ().nameBool = true;
+
+		}
+		for (int id = 24; id < 33; id++) {
+			tiles [randomNumbers[id]].GetComponent<Tile> ().cardValue = id-24; 
+		}
+		for (int id = 33; id < 42; id++) {
+			tiles [randomNumbers[id]].GetComponent<Tile> ().cardValue = id-33; 
+			tiles [randomNumbers[id]].GetComponent<Tile> ().nameBool = true;
+
+		}
+		for (int id = 42; id < 48; id++) {
+			tiles [randomNumbers[id]].GetComponent<Tile> ().cardValue = id-42; 
+		}
+		for (int id = 48; id < 54; id++) {
+			tiles [randomNumbers[id]].GetComponent<Tile> ().cardValue = id-48; 
+			tiles [randomNumbers[id]].GetComponent<Tile> ().nameBool = true;
+
+		}
+
+
+
+
+
+
+		////////////////////// Old Without Random from First Version /////////////
+	
+
+//
+//		for (int id = 0; id < 12; id++) {
+//			tiles [id].GetComponent<Tile> ().cardValue = id; 
+//		}
+//		for (int id = 12; id < 24; id++) {
+//			tiles [id].GetComponent<Tile> ().cardValue = id-12; 
+//			tiles [id].GetComponent<Tile> ().nameBool = true;
+//
+//		}
+//		for (int id = 24; id < 33; id++) {
+//			tiles [id].GetComponent<Tile> ().cardValue = id-24; 
+//		}
+//		for (int id = 33; id < 42; id++) {
+//			tiles [id].GetComponent<Tile> ().cardValue = id-33; 
+//			tiles [id].GetComponent<Tile> ().nameBool = true;
+//
+//		}
+//		for (int id = 42; id < 48; id++) {
+//			tiles [id].GetComponent<Tile> ().cardValue = id-42; 
+//		}
+//		for (int id = 48; id < 54; id++) {
+//			tiles [id].GetComponent<Tile> ().cardValue = id-48; 
+//			tiles [id].GetComponent<Tile> ().nameBool = true;
+//
+//		}
+
+
+
+
+		//		for (int id = 0; id < 1; id++) {
+		//			for (int i = 1; i < 14; i++) {
+		//
+		//				bool test = false;
+		//				int choice = 0;
+		//		//		while (!test) {
+		//		//			choice = Random.Range (0, cards.Length);
+		//		//			test = true;
+		//					//test = !(cards [choice].GetComponent<Card> ().intitialzed);
+		//		//		}
+		//				cards [i-1].GetComponent<Card> ().cardValue = i; 
+		//				cards [i-1].GetComponent<Card> ().intitialzed = true;
+		//			}
+		//		}
+		//		for (int id = 0; id < 27; id++) {
+		//			for (int i = 1; i < 14; i++) {
+		//
+		//				bool test = false;
+		//				int choice = 0;
+		//				//		while (!test) {
+		//				//			choice = Random.Range (0, cards.Length);
+		//				//			test = true;
+		//				//test = !(cards [choice].GetComponent<Card> ().intitialzed);
+		//				//		}
+		//				cards [i-1].GetComponent<Card> ().cardValue = i; 
+		//				cards [i-1].GetComponent<Card> ().intitialzed = true;
+		//			}
+		//		}
+
+		foreach (GameObject c in tiles)
+			c.GetComponent<Tile> ().setupGraphics ();
+
+		if (!_init)
+			_init = true;
+	}
+
+	public Sprite getTileBack() {
+		return tileBack;
+	}
+
+	public Sprite getTileFace(int i ) {
+		return tileFace [i];
+	}
+	public Sprite getTileName(int i ) {
+		return tileName [i];
+	}
+	void checkCards() {
+		List<int> c = new List<int> ();
+
+		for (int i = 0; i < tiles.Length; i++) { 
+			if (tiles [i].GetComponent<Tile> ().state == 0)
+				c.Add (i);
+		}
+
+		if (c.Count == 2)
+			cardCompariosn (c);
+	}
+
+	void cardCompariosn (List<int> c ) {
+		Tile.DO_NOT = true; 
+
+		int x = 1; 
+		if (tiles [c [0]].GetComponent<Tile> ().cardValue == tiles [c [1]].GetComponent<Tile> ().cardValue  && tiles [c [0]].GetComponent<Tile> ().nameBool != tiles [c [1]].GetComponent<Tile> ().nameBool) {
+			x = 2;
+			_matches--;
+//			matchText.text = "Number of Matches: " + _matches;
+			if (_matches == 0)
+				SceneManager.LoadScene ("MainScene");
+		}
+
+		for (int i = 0; i < c.Count; i++) {
+			tiles [c [i]].GetComponent<Tile> ().state = x;
+			tiles [c [i]].GetComponent<Tile> ().falseCheck ();
+
+
+		}
+	}
+	
+
+}
