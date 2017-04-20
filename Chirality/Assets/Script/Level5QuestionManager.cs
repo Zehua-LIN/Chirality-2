87a0e979e9c63733a2ca4cd17ff2ea3e7ab50da7
Vivi <Vivi@Cai-Weis-MacBook-Pro.local>
2017-04-20 14:03:36 +1000

    Level 5 updated

---
+++ Chirality/Assets/Script/Level5QuestionManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System.IO;
+using LitJson;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Level5QuestionManager : MonoBehaviour
+{
+
+	public static Level5QuestionManager Instance = null;
+
+	[SerializeField] GameObject[] questionObjects;
+	[SerializeField] GameObject[] questionAnswerObjects;
+	[SerializeField] Canvas canvas;
+	[SerializeField] Text gameTitle;
+	[SerializeField] Text scoreNumberLabel;
+	[SerializeField] Button nextButton;
+	[SerializeField] int gameLevel;
+
+	private List<QuestionLevel5> questions = new List<QuestionLevel5>();
+	private JsonData questionData;
+
+	private int score = 0;
+	private GameObject currentQuestion;
+	private GameObject currentQuestionAnswer;
+	private QuestionLevel5 currentQuestionObject;
+	private gameStatus currentStatus = gameStatus.InGame;
+	public gameStatus CurrentStatus {
+		get {
+			return currentStatus;
+		}
+	}
+		
+
+	void Awake()
+	{
+		// singleton
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+	void Start()
+	{
+
+		string path = readJsonData(gameLevel);	
+		questionData = JsonMapper.ToObject(File.ReadAllText(path));
+
+		loadQuestions();
+		instantiateRandomQuestionToDisplay();	
+	}
+
+
+
+	// create the Question objects from the Questions.json and append them to the List<Question>
+	void loadQuestions()
+	{
+		for (int i = 0; i < questionData.Count; i++)
+		{
+			//Debug.Log("Question ID: " + (int)questionData[i]["id"]);
+			questions.Add(new QuestionLevel5((int)questionData[i]["id"],(int)questionData[i]["level"],(string)questionData[i]["code"],(string)questionData[i]["name"],(int)questionData[i]["numberOfButtons"],questionObjects[i],questionAnswerObjects[i],convertArray(questionData,i)));
+		}
+	}
+
+	// pick a random question from the List<Question> and display it
+	void instantiateRandomQuestionToDisplay() {
+		int randomNum = Random.Range(0,questions.Count); // random a question
+		currentQuestionObject = questions[randomNum]; 
+		currentQuestion = Instantiate(currentQuestionObject.gameObject,canvas.transform,false);	// instantiate the prefab
+		currentQuestionAnswer = Instantiate(currentQuestionObject.answerObject,canvas.transform,false);
+
+		// change the game status and deactivate the answer button
+		currentStatus = gameStatus.InGame;
+	}
+		
+
+
+
+	// helper methos to convert json array to normal List
+	List<string> convertArray(JsonData ary, int index) {
+		List<string> temp = new List<string>();
+		for(int i = 0; i < (int)ary[index]["numberOfButtons"]; i++) {
+			temp.Add((string)ary[index]["answer"][i]);
+		}
+		return temp;
+	}
+
+	// switch to the main scene
+	public void homeButtonPressed()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+		
+
+	string readJsonData(int level) {
+		string path = "";
+		string fileName;
+
+		switch (level)
+		{
+		case 5:
+			fileName = "Level_Five_Questions.json";
+			break;
+		default:
+			fileName = "";
+			break;
+		}
+
+		var temp = fileName.Split("."[0]);
+
+		if(Application.platform == RuntimePlatform.Android) {
+			string oriPath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+			WWW reader = new WWW(oriPath);
+			while(!reader.isDone) {}
+
+			string realPath = Application.persistentDataPath + "/" + temp[0];
+			System.IO.File.WriteAllBytes(realPath, reader.bytes);
+			path = realPath;
+		}else {
+			path = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+		}
+		return path;
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Chirality/Assets/Script/Model/QuestionLevel5.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionLevel5 {
+	public int ID {get; set;}
+	public int Level {get; set;}
+	public string code {get; set;}
+	public string name {get; set;}
+	public int numberOfButtons {get; set;}
+	public GameObject gameObject {get; set;}
+	public GameObject answerObject {get; set;}
+	public List<string> answer {get; set;}
+
+	public QuestionLevel5(int id, int level, string code, string name, int numOfButtons, GameObject gameObj, GameObject answerObj, List<string> answer) {
+		this.ID = id;
+		this.Level = level;
+		this.code = code;
+		this.name = name;
+		this.numberOfButtons = numOfButtons;
+		this.gameObject = gameObj;
+		this.answerObject = answerObj;
+		this.answer = answer;
+	}
+}
