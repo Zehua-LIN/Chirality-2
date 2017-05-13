7d5fd3426e65da6b3e56d6c966df662c177b0bce
amt1986 <amt1986@gmail.com>
2017-05-13 23:50:38 +1000

    Fix Update

---
+++ Chirality/Assets/Script/MenuManager.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField] GameObject settingPanel;
+	[SerializeField] GameObject infoPanel;
+	[SerializeField] GameObject backgroundMusic;	// this is the prefab
+	[SerializeField] GameObject soundEffect;
+	[SerializeField] Toggle backgroundMusicToggle;
+	[SerializeField] Toggle soundEffectToggle;
+	[SerializeField] Toggle leftHandModeToggle;
+	[SerializeField] GameObject levelTwoPanel;
+	[SerializeField] Animator scrollAnimation;
+	[SerializeField] Animator levelTwoSubMenu;
+	[SerializeField] Animator levelFourSubMenu;
+	[SerializeField] Button[] levelButtons;
+	[SerializeField] Button levelFourStandardButton;
+	[SerializeField] Button levelFourExtremeButton;
+	[SerializeField] Button levelTwoStandardButton;
+	[SerializeField] Button levelTwoTimeButton;
+	[SerializeField] Button levelTwoExtremeButton;
+
+	[SerializeField] Image[] medals;
+	[SerializeField] Sprite menuButtonSelected;
+	[SerializeField] Sprite menuButtonUnselected;
+
+	private GameObject backgroundMusicObject = null;	// this is the actual audio object in game
+	private GameObject soundEffectObject = null;
+	private Color textColorSelected;
+	private Color textColorUnselected;
+
+	void Start() {
+		settingPanel.SetActive(false);
+		
+		loadUserSetting();
+		loadMedals();
+		convertTextColor();
+	}
+
+
+	void loadUserSetting() {
+		// read user setting
+		backgroundMusicToggle.isOn = PlayerPrefsX.GetBool("Background_Music_Toggle",true);
+		soundEffectToggle.isOn = PlayerPrefsX.GetBool("Sound_Effect_Toggle",false);
+		leftHandModeToggle.isOn = PlayerPrefsX.GetBool("Left_Handle_Toggle",false);
+
+		configureBackgroundMusic();
+		configureSoundEffect();
+	}
+
+	public void loadGame(string level) {
+		SceneManager.LoadScene(level);
+	}
+
+	public void saveUserSetting(Toggle toggle) {
+		PlayerPrefsX.SetBool(toggle.gameObject.name,toggle.isOn);
+		configureBackgroundMusic();
+		configureSoundEffect();
+	}
+
+	public void toggleSetting() {
+		settingPanel.SetActive(!settingPanel.activeInHierarchy);
+	}
+
+	public void toggleInfoPanel() {
+		infoPanel.SetActive(!infoPanel.activeInHierarchy);
+	}
+
+	public void displayLevelTwoPanel() {
+		hideLevelFourPanel();
+		levelTwoSubMenu.SetBool("isHidden",false);
+		levelButtons[1].GetComponent<Image>().sprite = menuButtonSelected;		
+		levelButtons[1].GetComponentInChildren<Text>().color = textColorSelected;
+	}
+
+	public void hideLevelTwoPanel() {
+		levelTwoSubMenu.SetBool("isHidden",true);
+		levelButtons[1].GetComponent<Image>().sprite = menuButtonUnselected;
+		levelButtons[1].GetComponentInChildren<Text>().color = textColorUnselected;
+	}
+
+	public void displayLevelFourPanel() {
+		hideLevelTwoPanel();
+		levelFourSubMenu.SetBool("isHidden",false);
+		levelButtons[3].GetComponent<Image>().sprite = menuButtonSelected;		
+		levelButtons[3].GetComponentInChildren<Text>().color = textColorSelected;
+	}
+
+	public void hideLevelFourPanel() {
+		levelFourSubMenu.SetBool("isHidden",true);
+		levelButtons[3].GetComponent<Image>().sprite = menuButtonUnselected;
+		levelButtons[3].GetComponentInChildren<Text>().color = textColorUnselected;
+	}
+
+	public void stopScrolling() {
+		scrollAnimation.Stop();
+	}
+
+	void configureBackgroundMusic() {
+		// assign the audio object to local
+		if(backgroundMusicObject == null && GameObject.Find("BackgroundMusic(Clone)")) {
+				backgroundMusicObject = GameObject.Find("BackgroundMusic(Clone)");
+		}
+		
+		if(backgroundMusicToggle.isOn && !GameObject.Find("BackgroundMusic(Clone)")) {
+			// if toggle is on and there is no existing one, instantiate one from the prefab
+			backgroundMusicObject = Instantiate(backgroundMusic,Vector3.zero,Quaternion.identity);
+			DontDestroyOnLoad(backgroundMusicObject);
+		}else if(!backgroundMusicToggle.isOn) {
+			// mute background music 
+			if(backgroundMusicObject != null) {
+				Destroy(backgroundMusicObject);
+			}
+		}
+	}
+
+	void configureSoundEffect() {
+		if(soundEffectObject == null && GameObject.Find("SoundEffect(Clone)")) {
+			soundEffectObject = GameObject.Find("SoundEffect(Clone)");
+		}
+
+		if(soundEffectToggle.isOn && !GameObject.Find("SoundEffect(Clone)")) {
+			soundEffectObject = Instantiate(soundEffect,Vector3.zero,Quaternion.identity);
+			DontDestroyOnLoad(soundEffectObject);
+		}else if(!soundEffectToggle.isOn) {
+			if(soundEffectObject != null) {
+				Destroy(soundEffectObject);
+			}
+		}
+	}
+
+	public void resetScore() {
+		for(int i = 1; i <= 6; i++) {
+			PlayerPrefs.DeleteKey("Level_" + i + "_High_Percentage");
+			if(levelButtons[i-1].transform.childCount > 1) {
+				Destroy(levelButtons[i-1].transform.GetChild(1).gameObject);
+			}
+		}
+		
+		
+	}
+
+	void loadMedals() {
+		for(int i = 1; i <= 6; i++) {
+			if(PlayerPrefs.HasKey("Level_" + i + "_High_Percentage")) {
+				float highest = PlayerPrefs.GetFloat("Level_" + i + "_High_Percentage");				
+				int medalNumber = getMedal(highest);
+				Image medal = Instantiate(medals[medalNumber],levelButtons[i-1].transform,false);
+				medal.rectTransform.sizeDelta = new Vector2(70,70);
+				medal.transform.localPosition = new Vector2(200,0);				
+			}					
+		}		
+
+		// lvl 4 sub medals
+		if(PlayerPrefs.HasKey("Level_4_Standard_High_Percentage")) {
+			float highest = PlayerPrefs.GetFloat("Level_4_Standard_High_Percentage");				
+			int medalNumber = getMedal(highest);
+			Image medal = Instantiate(medals[medalNumber],levelFourStandardButton.transform,false);
+			medal.rectTransform.sizeDelta = new Vector2(70,70);
+			medal.transform.localPosition = new Vector2(200,0);	
+		}
+		if(PlayerPrefs.HasKey("Level_4_Standard_Extreme_Percentage")) {
+			float highest = PlayerPrefs.GetFloat("Level_4_Extreme_High_Percentage");				
+			int medalNumber = getMedal(highest);
+			Image medal = Instantiate(medals[medalNumber],levelFourStandardButton.transform,false);
+			medal.rectTransform.sizeDelta = new Vector2(70,70);
+			medal.transform.localPosition = new Vector2(200,0);	
+		}
+	}
+
+	int getMedal(float score) {
+		if(score < 0.5f) {
+			return 0;
+		}else if(score >= 0.5f && score <= 0.69f) {
+			return 1;
+		}else if(score >= 0.7f && score <= 0.89f) {
+			return 2;
+		}else if(score >= 0.9f && score <= 0.99f) {
+			return 3;
+		}else {
+			return 4;
+		}
+	}
+
+	void convertTextColor() {
+		textColorUnselected = Color.white;
+		textColorSelected = new Color();
+		ColorUtility.TryParseHtmlString("#072B3BFF",out textColorSelected);
+	}
+}
---
+++ Chirality/Assets/Script/QuestionManager.cs
@@ -0,0 +1,299 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System.IO;
+using LitJson;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// to keep track of the game status
+public enum gameStatus
+{ InGame, InCheck, InFunFact }
+
+public class QuestionManager : MonoBehaviour {
+
+	public static QuestionManager Instance = null;
+
+	[SerializeField] GameObject[] questionObjects;
+	[SerializeField] GameObject[] questionAnswerObjects;
+	[SerializeField] GameObject deck;
+	[SerializeField] Canvas canvas;
+	[SerializeField] Text gameTitle;
+	[SerializeField] Text questionName;
+	[SerializeField] Text scoreNumberLabel;
+	[SerializeField] Button displayAnswerButton;
+	[SerializeField] GameObject helpPanel;
+	[SerializeField] GameObject funFactPanel;
+	[SerializeField] GameObject exitPanel;
+	[SerializeField] Button yesButton;
+	[SerializeField] Button noButton;
+	[SerializeField] Text funFactPanelText;
+	[SerializeField] int gameLevel;
+	[SerializeField] Button nextButton;
+
+	private List<Question> questions = new List<Question>();
+	private JsonData questionData;
+	private int score = 0;
+	private int numberOfQuestionsAnswred = 0;
+	private float totalNumberOfCells = 0f;
+	private bool soundEffectToggle;
+	private bool leftHandMode;
+	private GameObject currentQuestion;
+	private GameObject currentQuestionAnswer;
+	private Question currentQuestionObject;
+	private gameStatus currentStatus = gameStatus.InGame;
+	public gameStatus CurrentStatus {
+		get {
+			return currentStatus;
+		}
+	}
+	
+	private int Score {
+		get{
+			return score;
+		}
+		set{
+			score = value;
+			scoreNumberLabel.text = score.ToString() + "/" + totalNumberOfCells.ToString();
+		}
+	}
+
+	void Awake() {
+		// singleton
+		if (Instance == null) {
+			Instance = this;
+		}else if (Instance != this) {
+			Destroy(gameObject);
+		}
+	}
+
+
+	void Start () {
+		setUpHelpPanel();
+		// for testing
+		// PlayerPrefs.DeleteAll();
+
+		leftHandMode = PlayerPrefsX.GetBool("Left_Handle_Toggle",false);
+		if(leftHandMode) {
+			deck.transform.localPosition = new Vector2(-deck.transform.localPosition.x,0);
+		}
+		
+		string path = readJsonData(gameLevel);	
+		questionData = JsonMapper.ToObject(File.ReadAllText(path));
+
+		loadQuestions();
+		instantiateRandomQuestionToDisplay();		
+		StartCoroutine(configureNextButtonColor());
+	}
+
+	// create the Question objects from the Questions.json and append them to the List<Question>
+	void loadQuestions() {
+		for (int i = 0; i < questionData.Count; i++){
+			questions.Add(new Question((int)questionData[i]["id"],(int)questionData[i]["level"],(string)questionData[i]["code"],(string)questionData[i]["name"],(int)questionData[i]["numberOfCells"],(string)questionData[i]["facts"][Random.Range(0,10)],questionObjects[i],questionAnswerObjects[i],convertArray(questionData,i)));
+		}
+	}
+
+	// pick a random question from the List<Question> and display it
+	void instantiateRandomQuestionToDisplay() {
+		nextButton.image.color = Color.white;
+		int randomNum = Random.Range(0,questions.Count); // random a question
+		currentQuestionObject = questions[randomNum]; 
+		currentQuestion = Instantiate(currentQuestionObject.gameObject,canvas.transform,false);	// instantiate the prefab
+		currentQuestionAnswer = Instantiate(currentQuestionObject.answerObject,canvas.transform,false);
+		if(leftHandMode) {
+			currentQuestion.transform.localPosition = new Vector2(-currentQuestion.transform.localPosition.x + 30,0);
+			currentQuestionAnswer.transform.localPosition = new Vector2(-currentQuestionAnswer.transform.localPosition.x + 30,0);
+		}
+		currentQuestionAnswer.SetActive(false);
+		questionName.text = currentQuestionObject.name;
+		totalNumberOfCells += currentQuestionObject.numberOfCells; // record the number of cells for calculating result
+		
+		// change the game status and deactivate the answer button
+		currentStatus = gameStatus.InGame;
+		displayAnswerButton.gameObject.SetActive(false);
+	}
+
+	// configure the functions attached to this button based on different game status
+	public void nextButtonPressed() {
+		if(currentStatus == gameStatus.InGame) {
+			checkAnswer();
+		}else if(currentStatus == gameStatus.InCheck) {
+			displayFunFact();
+		}
+	}
+
+	void checkAnswer() {
+		// check for empty slots, return if there is empty one
+		// for(int i = 0; i < currentQuestion.transform.childCount; i++) {
+		// 	if(currentQuestion.transform.GetChild(i).childCount == 0) {
+		// 		return;
+		// 	}
+		// }
+		if(!checkForEmptyCells()) {
+			return;
+		}
+
+
+		// change the game status
+		currentStatus = gameStatus.InCheck;
+		displayAnswerButton.gameObject.SetActive(true); 
+		numberOfQuestionsAnswred += 1;	// to keep track of how many questions have been answered
+
+		// loop through the slots and check answer
+		for(int i = 0; i < currentQuestion.transform.childCount; i++) {
+			GameObject elementInCell = currentQuestion.transform.GetChild(i).GetChild(0).gameObject;		
+			if(elementInCell.tag == currentQuestionObject.answer[i]) {
+				plusScore();
+				elementInCell.transform.GetComponent<Image>().color = Color.green;
+			}else {
+				Score += 0;
+				elementInCell.transform.GetComponent<Image>().color = Color.red;
+			}		
+		}
+	}
+
+	public bool checkForEmptyCells() {
+		int count = 0;
+		for(int i = 0; i < currentQuestion.transform.childCount; i++) {
+			if(currentQuestion.transform.GetChild(i).childCount != 0) {
+				count ++;
+			}
+		}
+		return (count == currentQuestion.transform.childCount);
+	}
+
+	void plusScore() {
+		Score++;
+	}
+
+	// helper methos to convert json array to normal List
+	List<string> convertArray(JsonData ary, int index) {
+		List<string> temp = new List<string>();
+		for(int i = 0; i < (int)ary[index]["numberOfCells"]; i++) {
+			temp.Add((string)ary[index]["answer"][i]);
+		}
+		return temp;
+	}
+
+	// switch to the main scene
+	public void homeButtonPressed() {
+		exitPanel.SetActive(true);
+	}
+
+	public void yesButtonPressed() {
+		SceneManager.LoadScene("MainScene");
+	}
+
+	public void noButtonPressed() {
+		exitPanel.SetActive(false);
+	}
+
+	public void toggleHelpPanel() {
+		helpPanel.SetActive(!helpPanel.activeInHierarchy);
+	}
+
+	public void toggleAnswer() {
+		currentQuestion.SetActive(!currentQuestion.activeInHierarchy);
+		currentQuestionAnswer.SetActive(!currentQuestionAnswer.activeInHierarchy);		
+	}
+
+	public void funFactPanelTouched() {
+		if(numberOfQuestionsAnswred < 5) {
+			Destroy(currentQuestion);
+			Destroy(currentQuestionAnswer);
+			questions.Remove(currentQuestionObject);
+			funFactPanel.SetActive(false);
+			instantiateRandomQuestionToDisplay();
+		}else {
+			// go to game over scene 
+			PlayerPrefs.SetString("Game_Title",gameTitle.text);
+			float percetange = Mathf.Round((score/totalNumberOfCells)*100) / 100f;
+			PlayerPrefs.SetInt("Score",score);
+			PlayerPrefs.SetFloat("Percentage",percetange);
+
+			switch (gameLevel)
+			{
+				case 1:
+					if(!PlayerPrefs.HasKey("Level_1_High_Percentage")) {
+						PlayerPrefs.SetFloat("Level_1_High_Percentage",0f);
+					}
+					break;
+				case 3:
+					if(!PlayerPrefs.HasKey("Level_3_High_Percentage")) {
+						PlayerPrefs.SetFloat("Level_3_High_Percentage",0f);
+					}
+					break;
+				default:
+					break;
+			}
+			SceneManager.LoadScene("Game_Over_Scene");
+		}
+	}
+
+	void displayFunFact() {
+		funFactPanelText.text = currentQuestionObject.funFact;
+		funFactPanel.SetActive(true);
+		currentStatus = gameStatus.InFunFact;
+	}
+
+	string readJsonData(int level) {
+		string path = "";
+		string fileName;
+
+		switch (level)
+		{
+			case 1:
+				fileName = "Level_One_Questions.json";
+				break;
+			case 3:
+				fileName = "Level_Three_Questions.json";
+				break;
+			default:
+				fileName = "";
+				break;
+		}
+
+		var temp = fileName.Split("."[0]);
+
+		if(Application.platform == RuntimePlatform.Android) {
+			string oriPath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+			WWW reader = new WWW(oriPath);
+			while(!reader.isDone) {}
+
+			string realPath = Application.persistentDataPath + "/" + temp[0];
+  			System.IO.File.WriteAllBytes(realPath, reader.bytes);
+			path = realPath;
+		}else {
+			path = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+		}
+		return path;
+	}
+
+	IEnumerator configureNextButtonColor() {
+		while(true) {
+			nextButton.image.color = checkForEmptyCells() ? Color.cyan : Color.white;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+
+	void setUpHelpPanel() {
+		switch (gameTitle.text) {
+			case "Functional Groups":
+				if(PlayerPrefsX.GetBool("First_Time_Level_One",true)) {
+				helpPanel.SetActive(true);
+				PlayerPrefsX.SetBool("First_Time_Level_One",false);
+				}
+				break;
+			case "Intermolecular Forces":
+				if(PlayerPrefsX.GetBool("First_Time_Level_Three",true)) {
+				helpPanel.SetActive(true);
+				PlayerPrefsX.SetBool("First_Time_Level_Three",false);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+	
+	
+}
